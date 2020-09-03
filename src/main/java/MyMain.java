b67823d55d5a5409109aeb35fed20f35bd8176d1
amandacutrer <70414293+amandacutrer@users.noreply.github.com>
2020-09-02 21:40:21 -0700

    Update MyMain.java

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-
+        System.out.print("What is your name?); 
+                         System.out.println("Nice to meet you " + name");
+             
         scan.close();
     }
 }
