fe460ce63355ef1591d9e1f307bedb8b69a2481d
amandacutrer <70414293+amandacutrer@users.noreply.github.com>
2020-09-02 22:34:53 -0700

    Update MyMain.java

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,7 +4,19 @@ public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("What is your name?); 
-                         System.out.println("Nice to meet you " + name");
+        System.out.println("Nice to meet you " + name");
+        System.out.print("What is your favorite food?");
+        String food = scan.next();
+        System.out.println("Yummy! I love" + food);
+        System.out.print("What about your favorite animal?");
+        String animal = scan.next();
+        System.out.println("Oh cool I actually have a pet " + animal);
+        System.out.print("What do you like about them? ");
+        String reason = scan.next();
+        System.out.println("Me too! ");
+        System.out.print("We have a lot of common interests. Want to be friends?");
+        String friend = scan.next();
+        System.out.println("Ok");
              
         scan.close();
     }
